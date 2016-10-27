f3d0a30a3d6154a0189f6c9ae4daffce4a4077d2
Batuhan Baserdem <batuhan.baserdem@stonybrook.edu>
2016-10-27 23:02:01 +0000

    modified:   list_of_prime_numbers/main.py 	modified:   tests/test_main.py

--- list_of_prime_numbers/main.py
+++ list_of_prime_numbers/main.py
@@ -1,12 +1,45 @@
 """This is the entry point of the program."""
 
+# Sorry, got a head start 
+from math import sqrt
 
 def _is_prime(number):
-    pass
-
+    # 1 is not a prime number, so start with 2
+    i = 2
+    while i <= sqrt(number):
+        # if the number is divisible by any number lower than its sqrt, not a prime
+        if number % i == 0:
+            return False
+        # check with next number
+        i += 1
+    # If reached here without exit, it is prime
+    return True
 
 def list_of_prime_numbers(max_number):
-    pass
+    # build sieve of erysthothanes(sp?)
+    sieve = [0] * (max_number+1)
+    # manually mark 0 and 1 as not prime
+    if max_number < 2:
+        return []
+    
+    [sieve[0], sieve[1]] = [1, 1]
+    output = []
+    
+    # loop over all elements and mark off the primes
+    for index in range(max_number+1):
+        # reiterate if the number selected is not prime
+        if sieve[index] != 0:
+            continue
+        
+        # Add to output if prime
+        output.append(index)
+        
+        # Mark off multiples in sieve
+        for i in range(index, max_number+1, index):
+            sieve[i] += 1
+    
+    return output
 
 if __name__ == '__main__':
-    print(_is_prime(19))
+    print(_is_prime(4))
+    print(list_of_prime_numbers(100))
--- tests/test_main.py
+++ tests/test_main.py
@@ -28,10 +28,10 @@ class TestListOfPrimeNumbers(unittest.TestCase):
     def test_big_number_list(self):
         self.assertEqual(
             list_of_prime_numbers(19),
-            [1, 2, 3, 5, 7, 11, 13, 17, 19])
+            [2, 3, 5, 7, 11, 13, 17, 19])
 
     def test_list_one(self):
-        self.assertEqual(list_of_prime_numbers(1), [1])
+        self.assertEqual(list_of_prime_numbers(1), [])
 
     def test_list_zero(self):
         self.assertEqual(list_of_prime_numbers(0), [])
