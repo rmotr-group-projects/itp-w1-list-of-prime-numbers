fc7ccfd62918c05d6c79c5639a0838c535bc88f7
Ahmed Eid <a.eid@yandex.com>
2016-10-27 22:43:59 +0000

    done

--- list_of_prime_numbers/main.py
+++ list_of_prime_numbers/main.py
@@ -2,11 +2,18 @@
 
 
 def _is_prime(number):
-    pass
+    for i in range(2 , number):
+        if number % i == 0 :
+            return False
+    return True
 
 
 def list_of_prime_numbers(max_number):
-    pass
+    list = []
+    for i in range(1 , max_number + 1):
+        if _is_prime(i):
+            list.append(i)
+    return list
 
 if __name__ == '__main__':
     print(_is_prime(19))
