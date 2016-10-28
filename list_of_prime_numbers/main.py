a0ea6c1f2b5cf1313f3c369b438d54fde03ef6dd
Nikhila Yadunath <nikihapy@gmail.com>
2016-10-28 00:27:00 +0000

    Main functionality complete

--- list_of_prime_numbers/main.py
+++ list_of_prime_numbers/main.py
@@ -2,11 +2,26 @@
 
 
 def _is_prime(number):
-    pass
+    for i in range(2, number):
+        if number % i == 0:
+            return False 
+    
+    else:
+        return True
 
 
 def list_of_prime_numbers(max_number):
-    pass
+    results_list = []
+    for i in range(1, max_number+1):
+        if _is_prime(i) is True:
+            results_list.append(i) 
+            
+    return results_list
+    
+   
+
+    
+
 
 if __name__ == '__main__':
-    print(_is_prime(19))
+   print(list_of_prime_numbers(19))
