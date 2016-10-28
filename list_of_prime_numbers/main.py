9a032943c9bce711c5a6641b90f055566664aeae
mohammad <normalguyinvancouver@gmail.com>
2016-10-28 22:35:25 +0000

    adjustments to make algorithm more efficient. improved readability with flag.

--- list_of_prime_numbers/main.py
+++ list_of_prime_numbers/main.py
@@ -1,18 +1,18 @@
 """This is the entry point of the program."""
 
-
 def _is_prime(number):
     
-    if number <= 1: #edge cases
-         return False
+    is_prime = True # flag 
     
-    for divisor in range(2,number):
-        if (number % divisor == 0):
-            return False
-    return True
+    if number < 2: # numbers less than two are not considered to be prime
+        is_prime = False
     
-    
-
+    for factor in range(2,int(number ** 0.5) + 1): #search for factors can stop at this point
+        if (number % factor == 0):
+            is_prime = False
+            break # stop searching if number is not a prime 
+            
+    return is_prime
 
 def list_of_prime_numbers(max_number):
     
@@ -20,11 +20,10 @@ def list_of_prime_numbers(max_number):
     
     for prime in range(2,max_number+1):
         if _is_prime(prime): 
-            prime_numbers.append(prime)
+            prime_numbers.append(prime) # add the prime number to list
     
     return prime_numbers
         
-        
-
 if __name__ == '__main__':
-    print(_is_prime(1))
+    print(_is_prime(19))
+    print(list_of_prime_numbers(100000))
