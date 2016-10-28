e0c19dfc8b2fdcc0327e78207d24444b9560a7b7
Josh <joshkulchin@gmail.com>
2016-10-28 00:06:28 +0000

    G5 - Solution for Prime Numbers

---
+++ list_of_prime_numbers/.~c9_invoke_pt5DMT.py
@@ -0,0 +1,40 @@
+"""This is the entry point of the program."""
+
+# def prime(num):
+#   for i in range(2,num+1):
+#       if num % i:     #if it has a remainder
+#           continue
+#       elif num / i > 1:   # if the numerator/denominator > 1
+#           break           
+#       else:
+#           print(num)
+
+#10%5 = 0  10/5 = 2  10/10 = 1
+
+def _is_prime(number):
+    if number == 1:
+        return False
+    
+    for i in range(2,number+1):
+        p number % i
+            if n
+            
+        #     if isinstance((number / i), int):
+        #         return i
+        #         return False
+        #     else:
+        #         return True
+        
+        
+print (_is_prime(11))
+print (_is_prime(10))
+
+
+
+
+def list_of_prime_numbers(max_number):
+    pass
+'''
+if __name__ == '__main__':
+    print(_is_prime(19))
+'''
--- list_of_prime_numbers/main.py
+++ list_of_prime_numbers/main.py
@@ -1,12 +1,27 @@
-"""This is the entry point of the program."""
-
 
 def _is_prime(number):
-    pass
+    if number == 1:
+        return False
+    if number == 2:
+        return True
+    for i in range(2, number):
+        if number % i == 0:
+            return False
+    return True
 
 
 def list_of_prime_numbers(max_number):
-    pass
+    list_of_primes = []
+    for x in range(1,max_number+1):
+        if _is_prime(x) == True:
+            list_of_primes.append(x)
+        
+    return list_of_primes
+
 
 if __name__ == '__main__':
     print(_is_prime(19))
+
+
+
+
