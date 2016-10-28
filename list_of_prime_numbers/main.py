0e27307240b76f29042b2b8e030d871d1a6861ea
mohammad <normalguyinvancouver@gmail.com>
2016-10-28 00:07:46 +0000

    Main functionality built. All tests passing.

--- list_of_prime_numbers/main.py
+++ list_of_prime_numbers/main.py
@@ -2,11 +2,29 @@
 
 
 def _is_prime(number):
-    pass
+    
+    if number <= 1: #edge cases
+         return False
+    
+    for divisor in range(2,number):
+        if (number % divisor == 0):
+            return False
+    return True
+    
+    
 
 
 def list_of_prime_numbers(max_number):
-    pass
+    
+    prime_numbers = [] 
+    
+    for prime in range(2,max_number+1):
+        if _is_prime(prime): 
+            prime_numbers.append(prime)
+    
+    return prime_numbers
+        
+        
 
 if __name__ == '__main__':
-    print(_is_prime(19))
+    print(_is_prime(1))
--- tests/test_main.py
+++ tests/test_main.py
@@ -12,7 +12,7 @@ class TestIsPrime(unittest.TestCase):
         self.assertEqual(_is_prime(20), False)
 
     def test_one_prime(self):
-        self.assertEqual(_is_prime(1), True)
+        self.assertEqual(_is_prime(1), False)
 
     def test_two_prime(self):
         self.assertEqual(_is_prime(2), True)
@@ -28,10 +28,10 @@ class TestListOfPrimeNumbers(unittest.TestCase):
     def test_big_number_list(self):
         self.assertEqual(
             list_of_prime_numbers(19),
-            [1, 2, 3, 5, 7, 11, 13, 17, 19])
+            [2, 3, 5, 7, 11, 13, 17, 19])
 
     def test_list_one(self):
-        self.assertEqual(list_of_prime_numbers(1), [1])
+        self.assertEqual(list_of_prime_numbers(1), [])
 
     def test_list_zero(self):
         self.assertEqual(list_of_prime_numbers(0), [])
