5e514efe16106016747cb9db1fc1e294067b0cf2
Patrick Matthies <coding.newb80@gmail.com>
2017-01-14 17:12:08 +0000

    Solution

--- list_of_prime_numbers/main.py
+++ list_of_prime_numbers/main.py
@@ -1,12 +1,24 @@
 """This is the entry point of the program."""
 
 
-def _is_prime(number):
-    pass
-
+def _is_prime(num):
+    primes = []
+    if num <= 1 or isinstance(num,float):
+      return False
+    if num % 2 == 0 and num > 2:
+      return False
+    else:
+      for i in range(2,int(num**0.5)+1):
+        if num % i == 0:
+          return False
+      return True
 
 def list_of_prime_numbers(max_number):
-    pass
+    primes = []
+    for num in range(max_number + 1):
+        if _is_prime(num):
+          primes.append(num)
+    return primes
 
 if __name__ == '__main__':
     print(_is_prime(19))
