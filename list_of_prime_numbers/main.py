b38898c16b41f2070934a88051cb5e81a04b7ef1
Michael Giardullo <michaelgiardullo@gmail.com>
2017-01-15 21:07:05 +0000

    fully working solution

--- list_of_prime_numbers/main.py
+++ list_of_prime_numbers/main.py
@@ -2,11 +2,43 @@
 
 
 def _is_prime(number):
-    pass
+    isPrime = 0
+    prime_list = []
+    if number==0:
+        return "not a valid number"
+    if number==1:
+        return False
+
+    for x in range(number-2):
+        if (number % (x+2)) == 0:
+            isPrime = isPrime + 1
+
+    if isPrime == 0:
+        return True
+    else:
+        return False
 
 
 def list_of_prime_numbers(max_number):
-    pass
+    prime_list = []
+    counter = 1
+    while counter <= max_number:
+        if _is_prime(counter):
+            prime_list.append(counter)
+        counter += 1
+    return prime_list
+    
 
 if __name__ == '__main__':
     print(_is_prime(19))
+
+
+# def list_of_prime_numbers(max_number):
+# list = []
+# counter = 1
+# while counter < max_number:
+# if _is_prime(counter):
+# list.append(counter)
+# counter += 1
+# return list
+
