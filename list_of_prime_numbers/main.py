77f4f8d494de6e8181360f3efe1d2179fbca29dd
Mitesh Agrawal <mitesh.aw1@gmail.com>
2017-01-17 07:02:17 +0000

    Mitesh: Script to find primes

--- list_of_prime_numbers/main.py
+++ list_of_prime_numbers/main.py
@@ -2,11 +2,26 @@
 
 
 def _is_prime(number):
-    pass
+    if number>1:
+        for i in range(2,number):
+            if number % i == 0:
+                return False
+        else:
+            return True
 
 
 def list_of_prime_numbers(max_number):
-    pass
+    list=[]
+    if max_number>1:
+        for n in range(2,max_number+1):
+            if _is_prime(n):
+                list.append(n)
+                n += 1
+        return list
+    else:
+        list=[]
+        return list
 
 if __name__ == '__main__':
-    print(_is_prime(19))
+    print(_is_prime(33))
+    print(list_of_prime_numbers(0))
