321e004143a9ca2d3fa1a75bcf186b6a1fd79279
DRogers <dustinxrogers@gmail.com>
2017-01-19 01:39:14 +0000

    Prime number list maker completed!

--- list_of_prime_numbers/main.py
+++ list_of_prime_numbers/main.py
@@ -1,12 +1,28 @@
-"""This is the entry point of the program."""
+def _is_prime(num):
+    if num == 1:
+        return True
+        
+    elif num == 2 or num == 3 or num == 5 or num == 7:
+        return True
+        
+    else:
+        prime = False
+        for i in range(2, 9):
+            if num % i == 0:
+                prime = False
+                break
+
+            else:
+                i += 1
+                prime = True
+        return prime
 
 
-def _is_prime(number):
-    pass
-
-
-def list_of_prime_numbers(max_number):
-    pass
-
-if __name__ == '__main__':
-    print(_is_prime(19))
+def list_of_prime_numbers(num):
+    primelist = []
+    for i in range(num+1):
+        if _is_prime(i) == True:
+            primelist.append(i)
+        else:
+            pass
+    return primelist
