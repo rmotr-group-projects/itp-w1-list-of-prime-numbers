2b63d56662875c73b35aa495c80cd53a2d88bb88
Matt Martinez <mattm1823@gmail.com>
2017-04-29 17:31:26 +0000

    this is the extra assignment

--- list_of_prime_numbers/main.py
+++ list_of_prime_numbers/main.py
@@ -2,11 +2,20 @@
 
 
 def _is_prime(number):
-    pass
+    for i in range(2,number):
+        if number%i == 0:
+            return False
+    return True
+        
+            
 
 
 def list_of_prime_numbers(max_number):
-    pass
-
+    prime_list = []
+    for i in range(2,max_number+1):
+         if _is_prime(i):
+            prime_list.append(i)
+    return prime_list
+    
 if __name__ == '__main__':
     print(_is_prime(19))
